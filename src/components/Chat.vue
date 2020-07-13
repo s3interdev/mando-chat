e24374d5192529c12d3b23d112cb1b715bfee453
Superior Software Solutions <s3interdev@gmail.com>
2020-07-13 15:08:28 +0300

    Creating the chat window.

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="container chat">
-    <h2>Mando Chat for {{ this.name }}</h2>
+    <h2 class="center teal-text">Mando Chat for {{ this.name }}</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">Name:</span>
+            <span class="grey-text text-darken-3">message</span>
+            <span class="grey-text time">time</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <input type="text" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,4 +29,17 @@ export default {
 </script>
 
 <style>
+.chat h2 {
+  font-size: 2.1em;
+  margin-bottom: 55px;
+}
+
+.chat span {
+  font-size: 1.3em;
+}
+
+.chat .time {
+  display: block;
+  font-size: 1.1em;
+}
 </style>
