2de9a427dca47e1519713a205a2f08f40d695551
Superior Software Solutions <s3interdev@gmail.com>
2020-07-13 14:19:22 +0300

    Passing Props via routes.

---
+++ src/components/Chat.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container chat">
+    <h2>Mando Chat for {{ this.name }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chat",
+  props: ["name"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
